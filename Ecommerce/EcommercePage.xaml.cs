249cd7207df63dc7948c4cb4921f99899d96067c
immyjosh <immanueljoshuapaul@gmail.com>
2018-03-02 13:14:06 +0530

    initial commit

---
+++ Ecommerce/EcommercePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Ecommerce
+{
+    public partial class EcommercePage : ContentPage
+    {
+        public EcommercePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
